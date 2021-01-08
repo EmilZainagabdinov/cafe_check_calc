65cb57c934078d11b1674090d3eff04812856045
Emil Zainagabdinov <screw192@gmail.com>
2021-01-08 16:33:05 +0600

    Импортировал все компоненты и контейнеры. Добавил проверку выбранного режима, для отображения соответствующего контейнера. Добавил стили.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, {useState} from 'react';
 import './App.css';
+import ModeSelectRadio from "./components/ModeSelectRadio/ModeSelectRadio";
+import EvenMode from "./containers/EvenMode/EvenMode";
+import IndividualMode from "./containers/IndividualMode/IndividualMode";
 
 const App = () => {
+  const [calcMode, setCalcMode] = useState(null);
+
+  let calcBlock = null;
+
+  if (calcMode === 'even') {
+    calcBlock = <EvenMode mode={calcMode} />;
+  } else if (calcMode === 'individual') {
+    calcBlock = <IndividualMode mode={calcMode} />;
+  }
+
+  const onRadioChange = e => {
+    setCalcMode(e.target.value);
+  };
+
   return (
-    <div className="App" />
+      <div className="App">
+        <ModeSelectRadio mode={calcMode} onRadioChange={onRadioChange} />
+        {calcBlock}
+      </div>
   );
 }
 
